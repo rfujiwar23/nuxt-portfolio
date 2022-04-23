6b6b6d2995f28703291bd6191a32d8a221f5fc32
Ryo Fujiwara <jjcproject@icloud.com>
2022-04-23 17:13:45 +0900

    more

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,98 +1,105 @@
 <template>
-    <div>
-       <nav class="bg-gray-800">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="flex items-center justify-between h-16">
-            <div class="flex items-center">
-              <NuxtLink to="/"> <img
-                  class="h-8 w-8"
-                  src="~/assets/images/my-logo.png"
-                  alt="Ryo Fujiwara"
-                ></NuxtLink>
-              <div class="hidden md:block">
-                <div class="ml-10 flex items-baseline">
-                  <NuxtLink to="/video" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">VIDEO</NuxtLink>
-                  <NuxtLink to="/web" class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">WEB</NuxtLink>
-                </div>
+  <div>
+    <nav class="bg-gray-800">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <div class="flex items-center">
+            <NuxtLink to="/">
+              <img
+                class="h-8 w-8"
+                src="~/assets/images/my-logo.png"
+                alt="Ryo Fujiwara"
+            /></NuxtLink>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline">
+                <NuxtLink
+                  to="/video"
+                  class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                  >VIDEO</NuxtLink
+                >
+                <NuxtLink
+                  to="/web"
+                  class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                  >WEB</NuxtLink
+                >
               </div>
             </div>
-            
-            <div class="-mr-2 flex md:hidden">
-              <button
-                @click="toggle"
-                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+          </div>
+
+          <div class="-mr-2 flex md:hidden">
+            <button
+              @click="toggle"
+              class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+            >
+              <svg
+                :class="[isOpen ? 'hidden' : 'block', 'h-6 w-6']"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
               >
-                <svg
-                  :class="[isOpen ? 'hidden' : 'block', 'h-6 w-6']"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-                <svg
-                  :class="[isOpen ? 'block' : 'hidden', 'h-6 w-6']"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  ></path>
-                </svg>
-              </button>
-            </div>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+              <svg
+                :class="[isOpen ? 'block' : 'hidden', 'h-6 w-6']"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
           </div>
         </div>
-        <div :class="[isOpen ? '' : 'hidden', 'md:hidden']">
-          <div class="px-2 pt-2 pb-3 sm:px-3">
-            <a
-              to="/WEB"
-              class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
-            >WEB SITES and APPS</a>
+      </div>
+      <div :class="[isOpen ? '' : 'hidden', 'md:hidden']">
+        <div class="px-2 pt-2 pb-3 sm:px-3">
+          <a
+            to="/WEB"
+            class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
+            >WEB SITES and APPS</a
+          >
 
-            <a
-              href="/VIDEO"
-              class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-            >VIDEO</a>
-          </div>
-          
+          <a
+            href="/VIDEO"
+            class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+            >VIDEO</a
+          >
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
+  </div>
 </template>
-
 
 <script>
 export default {
   data() {
     return {
-      isOpen: false
-    }
+      isOpen: false,
+    };
   },
   methods: {
-    toggle () {
-      this.isOpen = !this.isOpen
-    }
-  }
-}
+    toggle() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
 </script>
 
 <style scoped>
 nav {
-  position:fixed;
-  top:0;
-  left:0;
-  right:0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
 }
 </style>
-
-
