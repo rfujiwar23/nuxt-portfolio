22b6d040622c4b01ff1c988ad856e36db047f684
Ryo Fujiwara <jjcproject@icloud.com>
2022-06-03 19:16:37 +0900

    spelling error

--- components/IntroVideo.vue
+++ components/IntroVideo.vue
@@ -13,7 +13,7 @@
        videos on e-commerce and other in-store videos, and eventually got the opportunity to direct 
        the in-studio programming, of one project being a turning point in directing the programming 
        for the first time.  <br>Even though I changed jobs in my career,
-       creating videos and the skills that I've acquired have come to be as valuable,,being able to utilize my skills as an editor and overall visual storyteller.
+       creating videos and the skills that I've acquired have come to be as valuable, being able to utilize my skills as an editor and overall visual storyteller.
        In my recent job, I've been working as a developer and a web video creator (as well as commercials and a short programming on FUJI TV).
        <br>
        Creating home shopping videos made me a better UX designer. 
