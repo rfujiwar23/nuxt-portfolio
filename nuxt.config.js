8c875fd6ad8588586998712c07a2d5c73b390d7d
Ryo Fujiwara <jjcproject@icloud.com>
2022-06-04 14:45:18 +0900

    upload, almost done

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,8 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel:"stylesheet", type:"text/css", href:'https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,500;1,600&display=swap' }
+      // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,500;1,600&display=swap' }
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,400;1,500;1,600;1,700&family=Noto+Sans+JP:wght@100;500;700;900&family=Zen+Kaku+Gothic+New:wght@300;400;500;700&display=swap"}
     ]
   },
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,141 +1,163 @@
 <template>
   <div id="app">
-    
     <div class="container">
       <Navbar />
-      <div class="me-myself-ryo md:w-1/6">
-        <h2 class="text-center">RYO FUJIWARA</h2>
-        <div class="img">
+
+      <div class="lg:w-2/3 mx-auto text-center">
+        <div class="lg:w-1/6 md:w-1/5 sm:w-1/3 w-1/3 mx-auto my-5">
           <img
-                
-                src="~/assets/images/ryo-fujiwara.png"
-                alt="Ryo Fujiwara"
-            />
+            src="~/assets/images/ryo-fujiwara.png"
+            alt="Ryo Fujiwara"
+            style="border-radius: 50%"
+          />
         </div>
-        
+        <div>
+          <h1 class="text-white md:text-3xl text-xl">
+            <span class="bg-gray-800 bg-opacity-75 px-5">RYO FUJIWARA</span>
+          </h1>
+        </div>
+        <div>
+          <h2 class="my-3">
+            <span class="kanji bg-gray-800 bg-opacity-75 px-3">藤原亮</span>
+          </h2>
+        </div>
+        <div class="mt-5">
+          <h2 class="md:text-3xl text-3xl px-4 bg-gray-800 bg-opacity-75">
+            I am
+            <span class="typed-text">{{ typeValue }}</span>
+            <span class="cursor" :class="{ typing: typeStatus }">&nbsp;</span>
+          </h2>
+        </div>
       </div>
-    
-    <h1>I am 
-      <span class="typed-text">{{ typeValue }}</span>
-      <span class="cursor" :class="{'typing': typeStatus}">&nbsp;</span>
-    </h1>
-  </div>
+    </div>
   </div>
 </template>
 
 <script>
 // export default {};
-import { setTimeout } from 'timers';
-  export default {
-    data: () => {
-      return {
-        typeValue: '',
-        typeStatus: false,
-        typeArray: ['an UIUX Developer', 'a Video Creator', 'a Japanese Bilingual', 'like a Swiss Army Knife'],
-        typingSpeed: 200,
-        erasingSpeed: 100,
-        newTextDelay: 2000,
-        typeArrayIndex: 0,
-        charIndex: 0
+import { setTimeout } from "timers";
+export default {
+  data: () => {
+    return {
+      typeValue: "",
+      typeStatus: false,
+      typeArray: [
+        "an UIUX Developer",
+        "a Video Creator",
+        "a Japanese Bilingual",
+        "like a Swiss Army Knife",
+      ],
+      typingSpeed: 200,
+      erasingSpeed: 100,
+      newTextDelay: 2000,
+      typeArrayIndex: 0,
+      charIndex: 0,
+    };
+  },
+  methods: {
+    typeText() {
+      if (this.charIndex < this.typeArray[this.typeArrayIndex].length) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue += this.typeArray[this.typeArrayIndex].charAt(
+          this.charIndex
+        );
+        this.charIndex += 1;
+        setTimeout(this.typeText, this.typingSpeed);
+      } else {
+        this.typeStatus = false;
+        setTimeout(this.eraseText, this.newTextDelay);
       }
     },
-    methods: {
-      typeText() {
-        if(this.charIndex < this.typeArray[this.typeArrayIndex].length) {
-          if(!this.typeStatus)
-            this.typeStatus = true;
-          this.typeValue += this.typeArray[this.typeArrayIndex].charAt(this.charIndex);
-          this.charIndex += 1;
-          setTimeout(this.typeText, this.typingSpeed);
-        }
-        else {
-          this.typeStatus = false;
-          setTimeout(this.eraseText, this.newTextDelay);
-        }
-      },
-      eraseText() {
-        if(this.charIndex > 0) {
-          if(!this.typeStatus)
-            this.typeStatus = true;
-          this.typeValue = this.typeArray[this.typeArrayIndex].substring(0, this.charIndex - 1);
-          this.charIndex -= 1;
-          setTimeout(this.eraseText, this.erasingSpeed);
-        }
-        else {
-          this.typeStatus = false;
-          this.typeArrayIndex += 1;
-          if(this.typeArrayIndex >= this.typeArray.length)
-            this.typeArrayIndex = 0;
-          setTimeout(this.typeText, this.typingSpeed + 1000);
-        }
+    eraseText() {
+      if (this.charIndex > 0) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue = this.typeArray[this.typeArrayIndex].substring(
+          0,
+          this.charIndex - 1
+        );
+        this.charIndex -= 1;
+        setTimeout(this.eraseText, this.erasingSpeed);
+      } else {
+        this.typeStatus = false;
+        this.typeArrayIndex += 1;
+        if (this.typeArrayIndex >= this.typeArray.length)
+          this.typeArrayIndex = 0;
+        setTimeout(this.typeText, this.typingSpeed + 1000);
       }
     },
-    created() {
-      setTimeout(this.typeText, this.newTextDelay + 200);
-    }
-  }
+  },
+  created() {
+    setTimeout(this.typeText, this.newTextDelay + 200);
+  },
+};
 </script>
 
-
-
 <style scoped>
-
-
 #app {
-	 width: 100%;
-	 height: 100vh;
-	 display: flex;
-	 justify-content: center;
-	 align-items: center;
-   background:url(https://res.cloudinary.com/rfujiwar23/image/upload/v1654312548/portfolio/tokyo1.png) no-repeat;
-   background-position:center;
-   /* background-size: cover; */
-
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url(https://res.cloudinary.com/rfujiwar23/image/upload/v1654312548/portfolio/tokyo1.png)
+    no-repeat;
+  background-position: center;
+  /* background-size: cover; */
 }
- h1 {
-	 font-size: 3rem;
-	 font-weight: normal;
-   color:#fff;
+h2 {
+  /* font-size: 3rem; */
+  font-weight: normal;
+  color: #fff;
 }
- h1 span.typed-text {
-	 color: #82e00f;
+h2 span.typed-text {
+  color: #82e00f;
+  text-shadow: 1px 3px 6px rgb(150, 241, 37);
 }
- h1 span.cursor {
-	 display: inline-block;
-	 margin-left: 3px;
-	 width: 4px;
-	 background-color: #fff;
-	 animation: cursorBlink 1s infinite;
+h2 span.cursor {
+  display: inline-block;
+  margin-left: 3px;
+  width: 4px;
+  background-color: #fff;
+  animation: cursorBlink 1s infinite;
 }
- h1 span.cursor.typing {
-	 animation: none;
+h2 span.cursor.typing {
+  animation: none;
 }
- @keyframes cursorBlink {
-	 49% {
-		 background-color: #fff;
-	}
-	 50% {
-		 background-color: transparent;
-	}
-	 99% {
-		 background-color: transparent;
-	}
+@keyframes cursorBlink {
+  49% {
+    background-color: #fff;
+  }
+  50% {
+    background-color: transparent;
+  }
+  99% {
+    background-color: transparent;
+  }
 }
 
 .me-myself-ryo {
   font-size: 1.25em;
-  color:#fff;
+  color: #fff;
 }
 
-@media screen and (max-width:500px) {
+.kanji {
+  font-family: "Zen Kaku Gothic New", sans-serif;
+}
+
+@media screen and (max-width: 768px) {
+  /* .me-myself-ryo {
+    margin:0 auto;
+  } */
+}
+
+@media screen and (max-width: 500px) {
   h1 {
     font-size: 2em;
-    padding:0 3vw;
+    padding: 0 3vw;
   }
 
   .me-myself-ryo {
     padding: 0 3vw;
   }
 }
- 
 </style>
