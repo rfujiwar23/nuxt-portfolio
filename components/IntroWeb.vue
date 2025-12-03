797cdf548d9a08ced595f1c01518fe01995099ea
Ryo Fujiwara <46469955+rfujiwar23@users.noreply.github.com>
2025-12-03 15:55:08 +0900

    Update IntroWeb.vue

--- components/IntroWeb.vue
+++ components/IntroWeb.vue
@@ -3,12 +3,12 @@
     <SocialLinks />
     <h2>Summary</h2>
     <p>
-      Prior to 2019, I was a web designer, I eventually figured that I couldn't create some websites, as they were really web-apps, as my initial attempts at Vue.js and React.js were almost complete disaster. I attended Le Wagon Tokyo to educate myself on full-stack development, learning about HTML/CSS, Bootstrap, JavaScript ES6, SQL, git, GitHub, Heroku and Ruby on Rails, and creating AirBnb clone, and also creating an English-Speaking App with the team. I'm most comfortable with front-end development through my prior experience as a web designer.  I am a UIUX designer more, but my current job has called me to be a front-developer as well.  With programming, I am most comfortable with Vue.js/Nuxt.js, and Rails and PHP.  With my experience in web designs, I love using SASS and Tailwind.
+      Prior to 2019, I was a web designer, I eventually figured that I couldn't create some websites, as they were really web-apps, as my initial attempts at Vue.js and React.js were almost complete disaster. I attended Le Wagon Tokyo to educate myself on full-stack development, learning about HTML/CSS, Bootstrap, JavaScript ES6, SQL, git, GitHub, Heroku and Ruby on Rails, and creating AirBnb clone, and also creating an English-Speaking App with the team. I'm most comfortable with front-end development through my prior experience as a web designer.  I am a UIUX designer more, but my current job has called me to be a front-developer as well.  With programming, I am most comfortable with Vue.js/Nuxt.js, and Rails and PHP.  With my experience in web designs, I love using SASS and Tailwind. Because Nature (Springer Nature) products are hard to share, especially the internal apps made, I would have to explain to you if needed.
 <br><br>
 2019年以前はWebデザイナーとして働いていましたが、当時取り組んでいたサイトの中には、実質的には「Webアプリ」であるために、自分では作りきれないものがあることに気づきました。Vue.js や React.js に初めて挑戦したときは、ほぼ自滅するほどでした。<br>
 そこで、フルスタック開発を学ぶのが一番だとわかり、2019年の1月に Le Wagon Tokyo に通い、HTML/CSS、Bootstrap、JavaScript ES6、SQL、Git、GitHub、Heroku、Ruby on Rails を習得し、Airbnb のクローン制作や、チームで英会話アプリを開発。<br>
 以前にWebデザイナーとしての経験があるため、フロントエンド開発とUIUXデザインが最も得意です。現職はUIUXデザイナーとして入社しましたが、2023年末あたりから Springer Nature Group でプロダクトマネジメント寄りの仕事が増え、アクセシビリティ、ユーザージャーニー、ユーザーリサーチに基づいたページ開発に携わっています。
-プログラミングでは Vue.js / Nuxt.js、Rails、PHP が最も得意です。Webデザインの経験から、SASS や Tailwind を使うことも好んでいます。<br>そして何よりも、日常生活にも通じますが、ユーザーエクスペリエンス（UX）、デジタルトランスフォーメーション（DX）、ユーザーフィードバックを重視し、「わかりやすい（Web）プロダクト」を作ることができれば良いと思っています。
+プログラミングでは Vue.js / Nuxt.js、Rails、PHP が最も得意です。Webデザインの経験から、SASS や Tailwind を使うことも好んでいます。<br>そして何よりも、日常生活にも通じますが、ユーザーエクスペリエンス（UX）、デジタルトランスフォーメーション（DX）、ユーザーフィードバックを重視し、「わかりやすい（Web）プロダクト」を作ることができれば良いと思っています。下記のプロジェクトには、ウェブデザインとウェブアプリ制作（UIUX・フロントエンド・マネジメント系）をリストしています。Springer Nature Groupのプロジェクトは、多くは社外に共有するのも多くに禁じられているのもあるので、大変申し訳ないですが、口頭で説明するかもしれません。
     </p>
     <h2 class="mt-5">Skills</h2>
     <p>Mark-Up/Programming</p>
